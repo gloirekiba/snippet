399b062ef444f561f752cf8fe86241d6971eceae
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 23:29:23 +0200

    🏷️ update snippet interface

--- src/interfaces/snippet.d.ts
+++ src/interfaces/snippet.d.ts
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 
 export default interface ISnippet extends mongoose.Document {
   title: string;
+  owner: mongoose.Types.ObjectI | null;
+  content: string;
+  hits: number;
+  syntax: mongoose.Types.ObjectId | null;
   category: mongoose.Types.ObjectId;
   tags: string[];
   password: string;
-  content: string;
-  hits: number;
-  comments: mongoose.Types.ObjectId[] | null;
+  comments: mongoose.Types.ObjectId[];
   createdAt: Date;
   updatedAt: Date;
   expiresAt: Date | null;
   exposure: string;
   folder: mongoose.Types.ObjectId | null;
-  owner: mongoose.Types.ObjectI | null;
-  syntax: string;
   burnAfterReading: boolean;
 }
