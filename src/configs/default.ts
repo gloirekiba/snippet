5eb2fa5e1ef5d5ecb5c5523f680f608632131631
gloirekiba <gloirekiba21@gmail.com>
2022-10-17 00:10:33 +0200

    🔥 delete file

--- src/configs/default.ts
+++
@@ -1,5 +0,0 @@
-export const APP_NAME = "Snippet Manager";
-export const APP_DESCRIPTION =
-  "A simple snippet manager for developers to store and share code snippets with others in a secure way and in a single place without worrying about losing them or sharing them with others.";
-
-export const DEFAULT_AVATAR = "default-avatar.png";
