1cee11c707d38a80011218765f03822ecf31d196
gloirekiba <gloirekiba21@gmail.com>
2022-10-17 01:52:21 +0200

    💡 comment code

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,15 +5,15 @@ import userSchema from "../schemas/user.schema";
 
 const User = mongoose.model<IUser>("User", userSchema);
 
-User.prototype.toJSON = function () {
-  const user = this.toObject();
-  user.id = user._id;
+// User.prototype.toJSON = function () {
+//   const user = this.toObject();
+//   user.id = user._id;
 
-  delete user._id;
-  delete user.__v;
-  delete user.password;
+//   delete user._id;
+//   delete user.__v;
+//   delete user.password;
 
-  return user;
-};
+//   return user;
+// };
 
 export default User;
