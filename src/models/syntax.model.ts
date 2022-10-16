3114e941af9d79faea94c9c915038c758fce19ad
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 23:31:15 +0200

    🗃️ update syntax model

--- src/models/syntax.model.ts
+++ src/models/syntax.model.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
+import ISyntax from "../interfaces/syntax";
 import syntaxSchema from "../schemas/syntax.schema";
 
-const Syntax = mongoose.model("Syntax", syntaxSchema);
+const Syntax = mongoose.model<ISyntax>("Syntax", syntaxSchema);
 
 export default Syntax;
