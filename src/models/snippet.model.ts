9025dde73851b04a50204145d473999c87559f4d
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 23:31:04 +0200

    🗃️ update snippet model

--- src/models/snippet.model.ts
+++ src/models/snippet.model.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
+import ISnippet from "../interfaces/snippet";
 import snippetSchema from "../schemas/snippet.schema";
 
-const Snippet = mongoose.model("Snippet", snippetSchema);
+const Snippet = mongoose.model<ISnippet>("Snippet", snippetSchema);
 
 export default Snippet;
