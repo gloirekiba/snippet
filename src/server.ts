afcc5746760bdad4175c7a003e32bd6c76e5e734
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 10:25:35 +0200

    ♻️ refactor code

--- src/server.ts
+++ src/server.ts
@@ -10,18 +10,16 @@ import morgan from "morgan";
 
 const app: Application = express();
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
-
 app.use("/api/user", userRouter);
-
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
