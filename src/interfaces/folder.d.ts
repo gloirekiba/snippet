fdf92122c388530e937d255addb160f8172bf09e
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 22:27:05 +0200

    🏷️ update folder type interface

--- src/interfaces/folder.d.ts
+++ src/interfaces/folder.d.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 interface IFolder extends mongoose.Document {
   name: string;
-  user: mongoose.Types.ObjectId;
+  owner: mongoose.Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
   snippets: mongoose.Types.ObjectId[];
