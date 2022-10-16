e78074dd5f0dfb393a0d8bd76fb20f20536d969b
gloirekiba <gloirekiba21@gmail.com>
2022-10-17 01:18:59 +0200

    🗃️ add ressources

--- src/configs/admin.config.ts
+++ src/configs/admin.config.ts
@@ -1,13 +1,14 @@
 import AdminJS from "adminjs";
 import { Database, Resource } from "@adminjs/mongoose";
 
+import folderResource from "../resources/folder.resource";
 import snippetResource from "../resources/snippet.resource";
 import userResource from "../resources/user.resource";
 
 AdminJS.registerAdapter({ Database, Resource });
 
 const admin = new AdminJS({
-  resources: [userResource, snippetResource],
+  resources: [userResource, snippetResource, folderResource],
   rootPath: "/admin",
 });
 
