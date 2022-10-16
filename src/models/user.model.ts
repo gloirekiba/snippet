d71b9cbe3f49d06aa541e0e18a1d3e11c5d06a3c
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 10:26:59 +0200

    ♻️ refactor code

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,73 +1,7 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
-import { DEFAULT_AVATAR } from "../configs/default";
-
-import UserValidator from "../helpers/UserValidator";
-
-interface IUser extends mongoose.Document {
-  name: string;
-  email: string;
-  avatar: string;
-  website: string;
-  password: string;
-  createdAt: Date;
-  updatedAt: Date;
-  folders: mongoose.Types.ObjectId[];
-}
-
-const userSchema = new mongoose.Schema<IUser>({
-  name: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: [true, UserValidator.USERNAME_REQUIRED],
-    match: [UserValidator.USERNAME_REGEX, UserValidator.ERR_USERNAME_INVALID],
-  },
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: [true, UserValidator.EMAIL_REQUIRED],
-    match: [UserValidator.EMAIL_REGEX, UserValidator.ERR_EMAIL_INVALID],
-  },
-  avatar: {
-    type: String,
-    default: DEFAULT_AVATAR,
-  },
-  website: {
-    type: String,
-    trim: true,
-    match: [UserValidator.WEBSITE_REGEX, UserValidator.ERR_WEBSITE_INVALID],
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: true,
-    match: [UserValidator.PASSWORD_REGEX, UserValidator.ERR_PASSWORD_INVALID],
-  },
-  createdAt: {
-    type: Date,
-    immutable: true,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  folders: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: "Folder",
-  },
-});
-
-userSchema.pre("save", async function (next) {
-  const user = this;
-  if (user.isModified("password"))
-    user.password = await bcrypt.hash(user.password, 10);
-
-  next();
-});
+import IUser from "../interfaces/user";
+import userSchema from "../schemas/user.schema";
 
 const User = mongoose.model<IUser>("User", userSchema);
 
