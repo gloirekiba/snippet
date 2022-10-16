f5d10536770e63ddf31e432435dba6acfdd6a4fc
gloirekiba <gloirekiba21@gmail.com>
2022-10-17 00:09:40 +0200

    ♻️ refactor code

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import path from "path";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import AdminJS from "adminjs";
-import AdminJSExpress from "@adminjs/express";
 import express, { Application } from "express";
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
+import admin from "./configs/admin.config";
 import connectDB from "./db/connectDB";
+
+import adminRouter from "./routes/admin.route";
 import userRouter from "./routes/user.route";
 
 connectDB();
@@ -15,19 +16,11 @@ connectDB();
 const start = async () => {
   const app: Application = express();
 
-  const admin = new AdminJS({
-    databases: [],
-    rootPath: "/admin",
-  });
-
-  const adminRouter = AdminJSExpress.buildRouter(admin);
-  app.use(admin.options.rootPath, adminRouter);
-
   app.use(morgan("dev"));
   app.use(express.json());
-  app.use(express.static(path.join(__dirname, "public")));
   app.use(express.urlencoded({ extended: false }));
 
+  app.use(admin.options.rootPath, adminRouter);
   app.use("/api/user", userRouter);
 
   const PORT = process.env.PORT || 5000;
