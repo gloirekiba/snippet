dcee13db420678886717a8172ea81d57f28aab0e
gloirekiba <gloirekiba21@gmail.com>
2022-10-17 01:05:55 +0200

    ♻️ refactor code

--- src/configs/admin.config.ts
+++ src/configs/admin.config.ts
@@ -1,13 +1,14 @@
 import AdminJS from "adminjs";
 import { Database, Resource } from "@adminjs/mongoose";
 
+import snippetResource from "../resources/snippet.resource";
 import userResource from "../resources/user.resource";
 
 AdminJS.registerAdapter({ Database, Resource });
 
-const adminConfig = new AdminJS({
-  resources: [userResource],
+const admin = new AdminJS({
+  resources: [userResource, snippetResource],
   rootPath: "/admin",
 });
 
-export default adminConfig;
+export default admin;
