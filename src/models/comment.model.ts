b806cd5e196d0228b90c80cc499f52f11ba5e5bb
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 23:30:44 +0200

    🗃️ update comment model

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
+import IComment from "../interfaces/comment";
 import commentSchema from "../schemas/comment.schema";
 
-const Comment = mongoose.model("Comment", commentSchema);
+const Comment = mongoose.model<IComment>("Comment", commentSchema);
 
 export default Comment;
