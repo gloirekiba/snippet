90ca9e02999d34cf00bc0285238e0f5e8ed68ba8
gloirekiba <gloirekiba21@gmail.com>
2022-10-16 23:54:15 +0200

    ✨ implement adminjs to express

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
 import express, { Application } from "express";
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
@@ -8,19 +10,30 @@ dotenv.config({ path: path.join(__dirname, "../.env") });
 import connectDB from "./db/connectDB";
 import userRouter from "./routes/user.route";
 
-const app: Application = express();
-
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/api/user", userRouter);
-
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const start = async () => {
+  const app: Application = express();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  const admin = new AdminJS({
+    databases: [],
+    rootPath: "/admin",
+  });
+
+  const adminRouter = AdminJSExpress.buildRouter(admin);
+  app.use(admin.options.rootPath, adminRouter);
+
+  app.use(morgan("dev"));
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.urlencoded({ extended: false }));
+
+  app.use("/api/user", userRouter);
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(` App is running at http://localhost:${PORT} `);
+  });
+};
+
+start();
